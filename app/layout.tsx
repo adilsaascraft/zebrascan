22fbda11d5f0969bb64eee0c321f4c0785420ad6
adilsaascraft <adilsaascraft@gmail.com>
2026-03-02 16:35:40 +0530

    code done

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-         <Toaster richColors position="bottom-right" closeButton />
+         <Toaster richColors position="top-right" closeButton />
       </body>
     </html>
   );
